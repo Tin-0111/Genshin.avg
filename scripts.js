83124d766b11eacde6b6934e9c5e8ab31f17e24c
Tin-0111 <jayjay2007g@gmail.com>
2024-07-24 19:14:43 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,11 +1,15 @@
+function showAvgButtons() {
+    const buttonContainer = document.getElementById('buttonContainer');
+    if (buttonContainer.style.display === 'none' || buttonContainer.style.display === '') {
+        buttonContainer.style.display = 'flex';
+    } else {
+        buttonContainer.style.display = 'none';
+    }
+}
+
 function showImage(imageSrc) {
     const imageDisplay = document.getElementById('image-display');
     const displayedImage = document.getElementById('displayed-image');
     displayedImage.src = imageSrc;
     imageDisplay.style.display = 'block';
-}
-
-function showAvgButtons() {
-    const buttonContainer = document.getElementById('buttonContainer');
-    buttonContainer.style.display = 'flex';
 }
