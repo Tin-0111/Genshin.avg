72454b7403a184454a86e0bd7aad595de82c5bad
Tin-0111 <jayjay2007g@gmail.com>
2024-07-24 21:56:04 +0900

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,8 +1,1 @@
-function toggleAvgButtons() {
-    const buttonContainer = document.getElementById('buttonContainer');
-    if (buttonContainer.style.display === 'none' || buttonContainer.style.display === '') {
-        buttonContainer.style.display = 'flex';
-    } else {
-        buttonContainer.style.display = 'none';
-    }
-}
+.
